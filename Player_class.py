0f75f58161d2202262e19b946de6dca6a9509e89
khans7197 <47429588+khans7197@users.noreply.github.com>
2019-05-28 19:57:49 -0400

    Update Player_class.py
    Player image loading stuff

--- Player_class.py
+++ Player_class.py
@@ -1,3 +1,11 @@
 import pygame
 
-#class Player():
+class Player(pygame.sprite.Sprite):
+  
+   # Initializing the class
+    def __init__(self):
+        super().__init__()
+        
+        # self.image = pygame.Surface(screen size cords go here)
+        # self.image = pygame.image.load(player image goes here)
+        self.rect = self.image.get_rect()
