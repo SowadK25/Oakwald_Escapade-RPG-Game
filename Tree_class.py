c39d539a65d4cec2f1eee4bf01bdeb3523948bcd
Constantinos <boyic3133@wrdsb.ca>
2019-05-30 13:01:57 -0400

    almost have button function working. I HOPE I CAN COMPLETE TOMOROW

--- Tree_class.py
+++ Tree_class.py
@@ -10,14 +10,10 @@ class Tree(pygame.sprite.Sprite):
         Trees.append(self)
         self.rect = pygame.Rect(position[0], position[1], 50, 50)
         
-        
+
 '''import pygame
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BROWN = (139, 69, 19)
+
 
 pygame.init()
 
@@ -27,17 +23,13 @@ width = 750
 screen = pygame.display.set_mode((height, width))
 pygame.display.set_caption("Oakwald Escapade")
 
-small = pygame.font.SysFont("TimesNewRoman", 25)
-big = pygame.font.SysFont("TimesNewRoman", 50)
+
 
 screen.fill(WHITE)
 pygame.display.flip()
 
 
-def sentence(font, word, color, x, y):
-    text = font.render(word, True, color)
-    screen.blit(text, (x, y))
-    pygame.display.update()
+
 
 
 sentence(big, "Hello", RED, 500, 200)
