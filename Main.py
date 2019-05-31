d5220a36aee522e655f7129d5064d1a51e0094f3
Constantinos <boyic3133@wrdsb.ca>
2019-05-31 13:33:24 -0400

    BUTTON FUNCTIONS DOES NOT WORK RIGHT NOW JUST USING THIS SO I CAN PULL

--- Main.py
+++ Main.py
@@ -9,13 +9,21 @@ width = 750
 screen = pygame.display.set_mode((height, width))
 pygame.display.set_caption('Oakwald Escapade')
 # colours
-WHITE = [255, 255, 255]
-BLACK = [0, 0, 0]
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BROWN = (139, 69, 19)
 
-# for the button function
+# function for the word
 
-not_pressed = pygame.image.render()
-pressed_down = pygame.image.render()
+def sentence(font, word, color, x, y):
+    text = font.render(word, True, color)
+    screen.blit(text, (x, y))
+    pygame.display.update()
+
+small = pygame.font.SysFont("TimesNewRoman", 25)
+big = pygame.font.SysFont("TimesNewRoman", 50)
 
 
 # for the maps
@@ -24,22 +32,32 @@ Tree_y = 0
 Floor_x = 0
 Floor_y = 0
 
+
+# ignore this for now
+
+
+
+def objects(text, font):
+    textSurface = font.render(text, True, BLACK)
+    return textSurface, textSurface.get_rect()
+
+
 # for button function
+def button (text,x,y,w,h,active,inactive,action = None):
+    position = pygame.mouse.get_pos()
+    clicked = pygame.mouse.get_pressed()
 
+    if x+w > position[0] > x and y+h > position[1] > y:
+        pygame.draw.rect(screen, active, (x,y,w,h))
 
-def button(text, height1, width1, x, y):
-    pressed = None
-    mouse_position = pygame.mouse.get_pos()
-    mouse_pressed = pygame.mouse.get_pressed()
-
-    if x+width1 > mouse_position[0] and y+height1 > mouse_position[1]:
-        pygame.draw.rect(screen, pressed_down, (x, y, width1, height1))
-        if mouse_pressed[0] == 1 and pressed != None:
-            pressed()
+        if clicked[0] == 1 and action != None:
+            print('working') # what the button is going to do
     else:
-        pygame.draw.rect(screen, not_pressed, (x, y, width1, height1))
+        pygame.draw.rect(screen, inactive,(x,y,w,h))
 
-
+    TextSurf, TextRect = objects("WORKING" , big)
+    TextRect.center = ((x+(w/2)), (y+(h/2)))
+    screen.blit(TextSurf, TextRect)
 # For main loop
 close = True
 
@@ -146,6 +164,7 @@ while close:
         break
 
 
+
 for row in final_level:
     for col in row:
         if col == '1':
@@ -154,7 +173,6 @@ for row in final_level:
             Floor((Floor_x, Floor_y))
     Tree_y += 50
     Tree_x = 0
-
 
 while close:
     for event in pygame.event.get():
@@ -165,5 +183,5 @@ while close:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 quit()
-    screen.fill(WHITE)
+    button(100,100,20,20,GREEN,BROWN,None)
     pygame.display.flip()
