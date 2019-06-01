0a56e6e3eef15bf1b8d7e10805a99da68135ceb4
khans7197 <khans7197@wrdsb.ca>
2019-06-01 17:39:38 -0400

    Shooting class and player class done, player can shoot

--- Testing_file.py
+++ Testing_file.py
@@ -60,16 +60,19 @@ class Shoot(pygame.sprite.Sprite):
         super().__init__()
         # Class is being initialized
 
+        # Creating 10 by 10 square for bullet
         self.image = pygame.Surface([10, 10])
         self.image.fill(BLACK)
         self.rect = self.image.get_rect()
+
+        # Bullets position and speed is being set as a vector quantity
         self.position = Vector2(pos)
         self.vel = direction * 10
 
     def update(self):
         """Allow bullets to be moved"""
-        self.position += self.vel
-        self.rect.center = self.position
+        self.position += self.vel  # Bullet will move based on its velocity
+        self.rect.center = self.position  # Bullet rectangle is updated
 
 
 clock = pygame.time.Clock()
